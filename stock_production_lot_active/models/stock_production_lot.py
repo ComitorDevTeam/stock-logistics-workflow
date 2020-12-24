1321e331a50827be95e7b64ba96fa2fbb069b164
Pieter <pieterpaulussen@code-source.be>
2020-12-24 16:33:32 +0100

    [FIX] Backport stock_production_lot_active to V12

--- stock_production_lot_active/models/stock_production_lot.py
+++ stock_production_lot_active/models/stock_production_lot.py
@@ -1,10 +1,16 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockProductionLot(models.Model):
     _inherit = "stock.production.lot"
 
     active = fields.Boolean(string="Active", default=True)
+
+    @api.multi
+    def toggle_active(self):
+        """ Inverse the value of the field active on the records in self. """
+        for record in self:
+            record.active = not record.active
